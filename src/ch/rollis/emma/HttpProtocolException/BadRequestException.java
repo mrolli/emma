5461acc9b144e7478c566bb13604bcbfc6c35d26
Michael Rolli <michael@rollis.ch>
2012-04-29 12:47:36 +0200

    additional constuctors

--- src/ch/rollis/emma/HttpProtocolException/BadRequestException.java
+++ src/ch/rollis/emma/HttpProtocolException/BadRequestException.java
@@ -1,5 +1,15 @@
 package ch.rollis.emma.HttpProtocolException;
 
 public class BadRequestException extends HttpProtocolException {
+    public BadRequestException(String msg) {
+        super(msg);
+    }
 
+    public BadRequestException(Exception cause) {
+        super(cause);
+    }
+
+    public BadRequestException(String msg, Exception cause) {
+        super(msg, cause);
+    }
 }
--- src/ch/rollis/emma/HttpProtocolException/HttpProtocolException.java
+++ src/ch/rollis/emma/HttpProtocolException/HttpProtocolException.java
@@ -1,5 +1,15 @@
 package ch.rollis.emma.HttpProtocolException;
 
 public class HttpProtocolException extends Exception {
+    public HttpProtocolException(String msg) {
+        super(msg);
+    }
 
+    public HttpProtocolException(Exception cause) {
+        super(cause);
+    }
+
+    public HttpProtocolException(String msg, Exception cause) {
+        super(msg, cause);
+    }
 }
