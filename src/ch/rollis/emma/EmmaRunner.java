af69fc9c9ad1c3f4e5aa22878cf09a41237cb4ed
Michael Rolli <michael@rollis.ch>
2012-05-07 13:31:48 +0200

    Implement SSL sockets - renamed HttpServer to SocketListener - SocketListener is now configurable - Moved configuration stuff out of SocketListener
    Fixes #7

--- src/ch/rollis/emma/EmmaRunner.java
+++ src/ch/rollis/emma/EmmaRunner.java
@@ -1,14 +1,62 @@
 package ch.rollis.emma;
 
+import java.io.File;
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import ch.rollis.emma.context.ServerContext;
+import ch.rollis.emma.context.ServerContextManager;
+
 public class EmmaRunner {
     public static void main(String[] args) {
-        HttpServer emma;
+        Logger logger = Logger.getLogger("server.log");
 
+        // prepare SSL KeyStore
+        System.setProperty("javax.net.ssl.keyStore", "emma.keystore");
+        System.setProperty("javax.net.ssl.keyStorePassword", "geheim");
+
+        logger.log(Level.INFO, "Starting emma");
+
+        HashMap<Integer, Boolean> ports = new HashMap<Integer, Boolean>();
+        ports.put(8080, false);
+        ports.put(8443, true);
+
+        ServerContextManager scm = new ServerContextManager();
+        File docRoot = new File("./vhosts/default/public_html");
+        ServerContext con = new ServerContext("localhost", docRoot);
+        con.setDefaultContext(true);
         try {
-            emma = new HttpServer(8080);
-            emma.start();
-        } catch (HttpServerException e) {
+            scm.addContext(con);
+
+
+            ThreadGroup socketListeners = new ThreadGroup("Socket Listeners");
+
+            for (int port : ports.keySet()) {
+                SocketListener sl = new SocketListener(port, scm, logger);
+                sl.setSecured(ports.get(port));
+                Thread t = new Thread(socketListeners, sl, "SocketListener on port " + port);
+                t.start();
+            }
+
+            while (!Thread.currentThread().isInterrupted()) {
+                Thread.sleep(1000);
+            }
+
+            logger.log(Level.INFO, "Stopping emma");
+
+            Thread[] threads = new Thread[socketListeners.activeCount()];
+            socketListeners.enumerate(threads);
+            for (Thread t : threads) {
+                t.interrupt();
+                t.join();
+            }
+        } catch (Exception e) {
             e.printStackTrace();
+            System.exit(1);
         }
+
+        logger.log(Level.INFO, "Exiting emma");
+        System.exit(0);
     }
 }
---
+++ src/ch/rollis/emma/SocketListener.java
@@ -0,0 +1,89 @@
+package ch.rollis.emma;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.net.ssl.SSLServerSocketFactory;
+
+import ch.rollis.emma.context.ServerContextManager;
+
+/**
+ * SocketListener objects listen on a server socket for incoming client
+ * requests.
+ * 
+ * @author mrolli
+ */
+public class SocketListener implements Runnable {
+    private final int port;
+    private final Logger logger;
+    private final ServerContextManager scm;
+    private boolean sslSecured = false;
+
+    public SocketListener(int port, ServerContextManager scm, Logger logger) {
+        // move webserver configuration to file
+        this.port = port;
+        this.scm = scm;
+        this.logger = logger;
+    }
+
+    public void setSecured(boolean flag) {
+        sslSecured = flag;
+    }
+
+    public boolean isSecured() {
+        return sslSecured;
+    }
+
+    @Override
+    public void run() {
+        ServerSocket serverSocket = null;
+
+        logger.log(Level.INFO, "Now listening on port " + port);
+
+        try {
+            if (isSecured()) {
+                serverSocket = SSLServerSocketFactory.getDefault().createServerSocket(port);
+            } else {
+                serverSocket = new ServerSocket(port);
+            }
+            serverSocket.setSoTimeout(1000);
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Unable to open server socket on port " + port + ".", e);
+        }
+
+        // create a thread group for all request handling threads
+        ThreadGroup handlers = new ThreadGroup("HTTP Request handlers on port " + port);
+
+        while (!Thread.currentThread().isInterrupted()) {
+            Socket comSocket;
+            try {
+                comSocket = serverSocket.accept();
+                Thread th = new Thread(handlers, new HttpRequestHandler(comSocket, logger, scm));
+                th.setName("Request thread " + th.getId());
+                th.start();
+            } catch (SocketTimeoutException e) {
+                // do nothing, just give the while loop a chance to exit on
+                // interrupt
+            } catch (IOException e) {
+                logger.log(Level.SEVERE, "Error proccessing client request.", e);
+            }
+        }
+
+        try {
+            Thread[] threads = new Thread[handlers.activeCount()];
+            handlers.enumerate(threads);
+            for (Thread t : threads) {
+                t.interrupt();
+            }
+            serverSocket.close();
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Error closing server socket.", e);
+        }
+        logger.log(Level.INFO, "Stopped listening on port " + port);
+
+    }
+}
