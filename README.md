75d84ee8650393088e4de6e7d048359713007058
Michael Rolli <michael@rollis.ch>
2012-05-17 12:20:11 +0200

    Addition of license to source

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ emma
 ====
 
 Emma is a tiny little webserver in Java, hacked together in the context of an exercise in data communication lecture.
+
+
+
+This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Switzerland License. 
--- src/ch/rollis/emma/Emma.java
+++ src/ch/rollis/emma/Emma.java
@@ -1,3 +1,14 @@
+/**
+ * Copyright (c) 2012 Michael Rolli - github.com/mrolli/emma
+ * All rights reserved.
+ * 
+ * This work is licensed under the Creative Commons
+ * Attribution-NonCommercial-ShareAlike 3.0 Switzerland
+ * License. To view a copy of this license, visit
+ * http://creativecommons.org/licenses/by-nc-sa/3.0/ch/
+ * or send a letter to Creative Commons, 444 Castro Street,
+ * Suite 900, Mountain View, California, 94041, USA.
+ */
 package ch.rollis.emma;
 
 import java.io.BufferedReader;
